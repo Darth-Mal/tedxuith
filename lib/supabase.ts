de50e3102c4b650ce25d772a57f3980c2f2f462f
Victor Malomo <malomovictor@gmail.com>
2026-03-19 08:54:16 +0100

    supabase?

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
+// lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!, // still ok to use the public URL
+  process.env.SUPABASE_SERVICE_ROLE_KEY!, // use SERVICE_ROLE_KEY for server writes
 );
